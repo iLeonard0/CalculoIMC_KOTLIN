a4a83ec41224313bb20ab0aa66ce054b322f4c10
iLeonard0 <folleleonardoprogramming@gmail.com>
2024-06-20 09:46:09 -0300

    Calculo IMC KOTLIN

---
+++ app/src/test/java/unipar/edu/utfpr/calculaimc_ultimo/ExampleUnitTest.kt
@@ -0,0 +1,18 @@
+package unipar.edu.utfpr.calculaimc_ultimo
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        val imc = MainActivity.Calculo()
+        assertTrue("IMC calculado: ", imc.calculaIMC(80.0, 1.8) in 24.6 .. 24.7)
+    }
+}
